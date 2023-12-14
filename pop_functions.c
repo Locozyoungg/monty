f7cbaa9cb10005f0718c8533e156ffecfbaccabf
ShikoliAmos31 <amosshikoli@gmail.com>
2023-12-14 04:40:23 -0800

    pop opcode

--- pop_functions.c
+++ pop_functions.c
@@ -20,3 +20,20 @@ void pop(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = NULL;
 	free(temp);
 }
+/**
+ * print_stack_recursive - print all elements of the stack using recursion
+ * @stack: pointer to the stack
+ * @line_number: line number of the instruction
+ */
+void print_stack_recursive(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	if (*stack != NULL)
+	{
+		printf("%d\n", (*stack)->n);
+		pop(stack, line_number);
+		print_stack_recursive(stack, line_number);
+	}
+}
+
