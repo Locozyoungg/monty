c1c2bbc7ed8857f04fde7b0291a34ba478c70f74
Locozyoungg <collaustin27@gmail.com>
2023-12-14 17:08:23 -0800

    add

--- add_opcode.c
+++ add_opcode.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * add - adds the top two elements of the stack
- * @stack: double pointer to the beginning of the stack
- * @line_number: line number
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the Monty byte code file.
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n += (*stack)->n;
-	pop(stack, line_number);
+    stack_t *temp;
+
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = *stack;
+    (*stack)->next->n += (*stack)->n;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    free(temp);
 }
